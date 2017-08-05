b367b55dea2dc54d3079b84792b6591bc12d2624
saashimi <saavedra.kevin@gmail.com>
2017-08-04 21:50:18 -0700

    initial date parsing.

--- class_to_coord.py
+++ class_to_coord.py
@@ -11,65 +11,39 @@ def format_date(df_date):
     df_date['Days'] = df_date['Meeting_Times'].str.extract('([^\s]+)', expand=True).astype(str)
     df_date['Start_Date'] = df_date['Meeting_Dates'].str.extract('([^\s]+)', expand=True)
     df_date['End_Date'] = df_date['Meeting_Dates'].str.extract('(?<=-)(.*)(?= )', expand=True)
-
-
-    date_dict = {'M' : '2016/09/26', 'T': '2016/09/27', 'W': '2016/09/28', 'R': '2016/09/29', 'F': '2016/09/30', 'S': '2016/10/01', 'U':'2016/10/02'}
-
     df_date['Start_Time'] = df_date['Meeting_Times'].str.extract('(?<= )(.*)(?=-)', expand=True)
-    df_date['Start_Time'] = pd.to_datetime(df_date['Start_Time'], format='%H%M')
     df_date['End_Time'] = df_date['Meeting_Times'].str.extract('((?<=-).*$)', expand=True)
-    df_date['End_Time'] = pd.to_datetime(df_date['End_Time'], format='%H%M')
-    df_date['Day'] = ''
-
+    
+    date_dict = {'M' : '2016/09/26', 'T': '2016/09/27', 'W': '2016/09/28', 'R': '2016/09/29', 'F': '2016/09/30', 'S': '2016/10/01', 'U':'2016/10/02'}
+    days = ['M', 'T', 'W', 'R', 'F', 'S', 'U']
+    for day in days:
+        df_date['Day_{0}'.format(day)] = ''
     for index, row in df_date.iterrows():
         for day in row['Days']:
             try:
-                row['Day'] = date_dict[day]
-                print(row['Day'])
+                start_time = pd.to_datetime(date_dict[day] + row['Start_Time'], format='%Y/%m/%d%H%M')
+                end_time = pd.to_datetime(date_dict[day] + row['End_Time'], format='%Y/%m/%d%H%M')
+                df_date.loc[index, 'Day_{0}'.format(day)] = pd.date_range(start_time, end_time)
             except:
                 continue
+    #df_date['Start_Time'] = pd.to_datetime(df_date['Start_Time'], format='%H%M')
+    #df_date['End_Time'] = pd.to_datetime(df_date['End_Time'], format='%H%M')
 
     return df_date
-
-def format_xlist(df_xl):
-    """
-    revises % capacity calculations by using Max Enrollment instead of room capacity.  
-    """
-    df_xl['Cap_Diff'] = np.where(df_xl['Xlst'] != '', 
-                                   df_xl['Max_Enrl'].astype(int) - df_xl['Actual_Enrl'].astype(int), 
-                                   df_xl['Room_Capacity'].astype(int) - df_xl['Actual_Enrl'].astype(int)) 
-    df_xl = df_xl.loc[df_xl['Room_Capacity'].astype(int) < 999]
-
-    return df_xl 
-
 
 pd.set_option('display.max_rows', None)  
 filename = 'enrollment_data/PSU_master_classroom.csv'
 
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), filename), dtype={'Schedule': object, 'Schedule Desc': object})   
 df = df.fillna('')
-
-df = format_date(df)
-# Avoid classes that only occur on a single day
-df = df.loc[df['Start_Date'] != df['End_Date']]
 terms = [201604]
 df = df.loc[df['Term'].isin(terms)]
+
+df = format_date(df)
+
+# Avoid classes that only occur on a single day
+df = df.loc[df['Start_Date'] != df['End_Date']]
 df = df.loc[df['Online Instruct Method'] != 'Fully Online']
 
-# Calculate number of days per week and treat Sunday condition
-df['Days_Per_Week'] = df['Days'].str.len()
-df['Room_Capacity'] = df['Room_Capacity'].apply(lambda x: x if (x != 'No Data Available') else 0)
-df['Building'] = df['ROOM'].str.extract('([^\s]+)', expand=True)
-
-df_cl = format_xlist(df)
-
-# Normalize the results
-df_cl = df_cl.replace([np.inf, -np.inf], np.nan).dropna()
-
-#days = ['M', 'T', 'W', 'R', 'F', 'S', 'U']
-#for day in days:
-#    if day in days:
-
-
-#df_cl.to_csv('./test_output.csv')
+df.to_csv('./test_output.csv')
 
