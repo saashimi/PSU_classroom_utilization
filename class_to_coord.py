c99168283ddf1938ab203a805041050d0afb2add
saashimi <saavedra.kevin@gmail.com>
2017-08-05 06:18:01 -0700

    organize files and csv output handling.

--- class_to_coord.py
+++ class_to_coord.py
@@ -6,13 +6,17 @@ import os
 
 def format_date(df_date):
     """
-    Splits Meeting Times and Dates into datetime objects where applicable using regex.
+    Splits Meeting Times and Dates into datetime objects where applicable using 
+    regex. Adds separate columns for days of the weeks to facilitate separate 
+    csv file generation. 
     """
     df_date['Days'] = df_date['Meeting_Times'].str.extract('([^\s]+)', expand=True).astype(str)
     df_date['Start_Date'] = df_date['Meeting_Dates'].str.extract('([^\s]+)', expand=True)
     df_date['End_Date'] = df_date['Meeting_Dates'].str.extract('(?<=-)(.*)(?= )', expand=True)
     df_date['Start_Time'] = df_date['Meeting_Times'].str.extract('(?<= )(.*)(?=-)', expand=True)
     df_date['End_Time'] = df_date['Meeting_Times'].str.extract('((?<=-).*$)', expand=True)
+
+    df_date['Building'] =  df_date['ROOM'].str.extract('([^\s]+)', expand=True).astype(str)
     
     date_dict = {'M' : '2016/09/26', 'T': '2016/09/27', 'W': '2016/09/28', 'R': '2016/09/29', 'F': '2016/09/30', 'S': '2016/10/01', 'U':'2016/10/02'}
     days = ['M', 'T', 'W', 'R', 'F', 'S', 'U']
@@ -23,27 +27,38 @@ def format_date(df_date):
             try:
                 start_time = pd.to_datetime(date_dict[day] + row['Start_Time'], format='%Y/%m/%d%H%M')
                 end_time = pd.to_datetime(date_dict[day] + row['End_Time'], format='%Y/%m/%d%H%M')
-                df_date.loc[index, 'Day_{0}'.format(day)] = pd.date_range(start_time, end_time)
+                #df_date.loc[index, 'Day_{0}'.format(day)] = pd.date_range(start_time, end_time)
+                df_date.loc[index, 'Day_{0}'.format(day)] = start_time
             except:
                 continue
-    #df_date['Start_Time'] = pd.to_datetime(df_date['Start_Time'], format='%H%M')
-    #df_date['End_Time'] = pd.to_datetime(df_date['End_Time'], format='%H%M')
-
     return df_date
 
-pd.set_option('display.max_rows', None)  
-filename = 'enrollment_data/PSU_master_classroom.csv'
+def save_to_csv(df_final):
+    days = ['M', 'T', 'W', 'R', 'F', 'S', 'U']
+    for day in days:
+        df_final = df_final.loc[df_final['Day_{0}'.format(day)] != '']
+        columns = ['Building', 'Class', 'Actual_Enrl', 'Day_{0}'.format(day)]
+        df_final.to_csv('/map_output/classes_{0}.csv'.format(day), columns=columns)
 
-df = pd.read_csv(os.path.join(os.path.dirname(__file__), filename), dtype={'Schedule': object, 'Schedule Desc': object})   
-df = df.fillna('')
-terms = [201604]
-df = df.loc[df['Term'].isin(terms)]
+def main():
+    """
+    main program control.
+    """
+    pd.set_option('display.max_rows', None)  
+    filename = 'map_input/PSU_master_classroom.csv'
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), filename), dtype={'Schedule': object, 'Schedule Desc': object})   
+    df = df.fillna('')
 
-df = format_date(df)
+    terms = [201604]
+    df = df.loc[df['Term'].isin(terms)]
 
-# Avoid classes that only occur on a single day
-df = df.loc[df['Start_Date'] != df['End_Date']]
-df = df.loc[df['Online Instruct Method'] != 'Fully Online']
+    df = format_date(df)
 
-df.to_csv('./test_output.csv')
+    # Avoid classes that only occur on a single day
+    df = df.loc[df['Start_Date'] != df['End_Date']]
+    df = df.loc[df['Online Instruct Method'] != 'Fully Online']
+    save_to_csv(df)
+
+if __name__ == '__main__':
+    main()
 
